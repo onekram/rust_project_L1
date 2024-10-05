cbb5d2a08b17f08e7f76c67391c810dd4f629a3c
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-10-05 21:04:27 +0300

    feat: Add dashmap usage

--- src/l1_8.rs
+++ src/l1_8.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::thread;
 use std::sync::{Arc, Mutex};
+use dashmap::DashMap;
 
 fn hash_map_with_mutex(elements: Vec<(String, i32)>) -> HashMap<String, i32> {
     let map = Arc::new(Mutex::new(HashMap::new()));
@@ -26,9 +27,41 @@ fn hash_map_with_mutex(elements: Vec<(String, i32)>) -> HashMap<String, i32> {
     Arc::try_unwrap(map).expect("Failed to unwrap Arc").into_inner().expect("Failed to unlock Mutex")
 }
 
+fn dash_map(elements: Vec<(String, i32)>) -> DashMap<String, i32> {
+    let map = Arc::new(DashMap::new());
+    let mut handles = vec![];
+
+    for (id, (key, value)) in elements.iter().enumerate() {
+        let map_clone = Arc::clone(&map);
+        let key_clone = key.clone();
+        let value_clone = value.clone();
+
+        let handler = thread::spawn(move || {
+            map_clone.insert(key_clone, value_clone);
+            println!("Added element to map from thread {id}");
+        });
+        handles.push(handler);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    };
+
+    Arc::try_unwrap(map).expect("Failed to unwrap Arc")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test] 
+    fn dash_map_test() {
+        let arr = vec![("first".to_string(), 1), ("second".to_string(), 2), ("third".to_string(), 3)];
+        let map = dash_map(arr);
+
+        assert_eq!(*map.get(&"first".to_string()).unwrap(), 1);
+        assert_eq!(*map.get(&"second".to_string()).unwrap(), 2);
+    }
 
     #[test] 
     fn hash_map_with_mutex_test() {
