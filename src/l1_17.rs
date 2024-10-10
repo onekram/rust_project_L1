132197715a38a70b0d57452c2539f20fc3b4a6a7
Ivan Kramarenko <ivankramarenko13@gmail.com>
2024-10-10 09:14:41 +0300

    docs: Add comments

--- src/l1_17.rs
+++ src/l1_17.rs
@@ -2,15 +2,15 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn counter() -> usize {
-    let counter = Arc::new(Mutex::new(0usize));
+    let counter = Arc::new(Mutex::new(0usize));  // Put counter to arc mutes
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter_clone = Arc::clone(&counter);
+        let counter_clone = Arc::clone(&counter);  // Clone counter between threads
         let handle = thread::spawn(move || {
             for _ in 0..1000 {
-                let mut num = counter_clone.lock().unwrap();
-                *num += 1;
+                let mut num = counter_clone.lock().unwrap();  // Lock counter in current thread
+                *num += 1;  // Increment counter
             }
         });
         handles.push(handle);
